7fe866feac686fc590ac01b0a7f41f442e05217f
nikolaospanagopoulos <nikos4222@outlook.com.gr>
2024-06-01 19:59:38 +0300

    implementing labels

--- codegen.c
+++ codegen.c
@@ -1828,6 +1828,9 @@ void codegen_generate_goto_statement(struct node *node) {
   asm_push("jmp label_%s", node->stmt._goto.label->sval);
 }
 
+void codegen_generate_label(struct node *node) {
+  asm_push("label_%s:", node->label.name->sval);
+}
 void codegen_generate_statement(struct node *node, struct history *history) {
 
   switch (node->type) {
@@ -1872,6 +1875,9 @@ void codegen_generate_statement(struct node *node, struct history *history) {
     break;
   case NODE_TYPE_STATEMENT_GOTO:
     codegen_generate_goto_statement(node);
+    break;
+  case NODE_TYPE_LABEL:
+    codegen_generate_label(node);
     break;
   }
 
